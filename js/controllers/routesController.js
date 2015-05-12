ae12ac50d956881fb6dedc815cb6749b692756b2
dsbotta <danielbotta@Daniels-MacBook-Pro.local>
2015-05-12 16:02:34 -0400

    Integrated Code School Badges and  added logo

--- js/controllers/routesController.js
+++ js/controllers/routesController.js
@@ -15,6 +15,14 @@
 				controllerAs: 'myInfo'
 			})
 
+			.when ('/codeschool', {
+				templateUrl: 'routes/codeschool/',
+			})
+
+			.when ('/codeschool-badge/:badgeName', {
+				templateUrl: 'routes/codeschool/codeschool-badge/',
+			})
+
 		}]);
 
 })();
--- js/controllers/treehouse-ajax-controller.js
+++ js/controllers/treehouse-ajax-controller.js
@@ -3,12 +3,16 @@
 	angular.module('CodingReportCard')
 		.controller('TreehouseAjax', ['$scope', '$http', function($scope, $http) {
 			var treehouse = this;
-			 
 
 			$http.get('http://teamtreehouse.com/danielbotta.json', {cache: true})
 				.success(function(data) {
-					$scope.profile = data;
+					$scope.profileT = data;
 				})
+
+			$http.jsonp('https://www.codeschool.com/users/dsbotta.json?callback=JSON_CALLBACK', {cache: true})
+				.success(function(data) {
+					$scope.profileC = data;
+				})	
 
 		}]);
 
@@ -53,5 +57,49 @@
 
 
 		}]);
+
+
+		// angular.module('CodingReportCard')
+		// .controller('CodeSchoolBadgeAjax', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+		// 	var codeSchool = this;
+		// 	var badgeName = $routeParams.badgeName;
+		// 	var next = null;
+		// 	var prev = null;
+		// 	$scope.badge = null;
+		// 	$scope.nextBadge = null;
+		// 	$scope.prevBadge = null;
+
+		// 	function init() {
+		// 		for(var i in codeSchool.profile.badges) {
+		// 			console.log(codeSchool.profile.badges[i].name.replace(/\s/g, "%20"));
+		// 			if(codeSchool.profile.badges[i].name === badgeName) {
+		// 				$scope.badge = codeSchool.profile.badges[i];
+		// 				if(parseInt(i) + 1 === codeSchool.profile.badges.length) {
+		// 					next = 0;
+		// 				} else {
+		// 					next = parseInt(i) + 1;
+		// 				}
+		// 				if(parseInt(i) === 0) {
+		// 					prev = parseInt(codeScool.profile.badges.length) - 1;
+		// 				} else {
+		// 					prev = parseInt(i) + - 1;
+		// 				}
+		// 				$scope.prevBadge = codeSchool.profile.badges[prev];
+		// 				$scope.nextBadge = codeSchool.profile.badges[next];
+		// 				console.log($scope.nextBadge);
+		// 				break 
+		// 			}
+		// 		}
+		// 	};
+
+		// 	$http.jsonp('https://www.codeschool.com/users/dsbotta.json?callback=JSON_CALLBACK', {cache: true})
+		// 		.success(function(data) {
+		// 			codeSchool.profile = data;
+		// 			init();
+		// 	})
+
+
+
+		// }]);
 
 })();
