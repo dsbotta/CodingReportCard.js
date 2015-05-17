919733f9f88e8d323a27dba0d33d2a2070387fe9
dsbotta <danielbotta@Daniels-MacBook-Pro.local>
2015-05-17 18:45:54 -0400

    Added additional directive for the badges on each profile page

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>myCodingReportCard</h1>
+<h3>Author: <cite>Daniel Botta</cite></h3>
+</hr>
+<p>This is a Report Card to display badges earned through online coding schools <span><a href="http://teamtreehouse.com">Treehouse</a></span> and <span><a href="http://codeschool.com">Code School</a></span>.</p>
+
+<strong>How to Use</strong>
+<pre><p>
+	First, clone the repository using the link https://github.com/dsbotta/MyCodingReportCard.js.
+
+	Navigate to the ajax-controller.js file with the following path js/controllers/ajax-controllers.
+</p></pre>
--- js/directives/directives.js
+++ js/directives/directives.js
@@ -14,6 +14,32 @@
 				templateUrl: 'routes/directives/search-bar.html'
 			}
 		})
+
+		.directive('profileInfo', function() {
+			return {
+				restrict: 'E',
+				templateUrl: 'routes/directives/profile-info.html',
+				scope: {
+					school: '@',
+					image: '@',
+					name: '@',
+					num: '@',
+					points: '@'
+				}
+			}
+		})
+
+		.directive('allBadges', function() {
+			return {
+				restrict: 'E',
+				templateUrl: 'routes/directives/badges.html',
+				scope: {
+					link: '@',
+					name: '@',
+					icon: '@'
+				}
+			}
+		})
 	
 }
 )();
